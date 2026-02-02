b8d0ea5449f40c31b7d12785d8773394d9c41f76
TaratipT <66070086@kmitl.ac.th>
2026-02-02 20:21:01 +0700

    c0 ex4+5 | c1 ex1

--- cell00/ex04/argv.sh
+++ cell00/ex04/argv.sh
@@ -1,13 +1,8 @@
-#!/bin/bash
-
-# Check if any arguments were provided
 if [ $# -eq 0 ]; then
     echo "No arguments supplied"
 else
-    # Counter to track how many we have printed
     count=1
     for arg in "$@"; do
-        # Exit the loop if we've already printed 3 arguments
         if [ $count -gt 3 ]; then
             break
         fi
